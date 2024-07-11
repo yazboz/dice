b080a5f75ea7ce2e2ea266c9c10df624a48290ec
Fatihhan <fatihhangulec@gmail.com>
2024-07-11 16:00:13 +0300

    ..

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 4545;
+const PORT = process.env.PORT || 3000;
 
 // Public klasörünü statik dosyalar için kullan
 app.use(express.static(path.join(__dirname, 'public')));
